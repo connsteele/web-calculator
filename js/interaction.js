8ba44aa65a8e100fc7d4a59fc514648afe7524d3
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-09 16:51:53 -0700

    Inputs display
    basic support and events for displaying the inputs to the calculator screen

--- js/interaction.js
+++ js/interaction.js
@@ -10,9 +10,36 @@ function delegateButtons(event){
     if (!element.matches("button"))
         return;
 
-    console.log(`${element.textContent} was clicked`);
+    const content = element.textContent;
+    console.log(`${content} was clicked`);
+
+    // Send off a custom event to update the display of the calculator
+    const update = new CustomEvent("update", {
+        bubbles : true,
+        detail : {value : content}
+        }
+    );
+    element.dispatchEvent(update);
+
 }
+
+// Update the calculators display to reflect input from user. Triggered by custom event.
+function updateDisplay(event) {
+    const value = event.detail.value; 
+    console.log("Display updated");
+    console.log(`${value}`)
+
+    const display = document.querySelector(".display");
+    display.textContent = value;
+    // if the value is 0 then just use the new value
+    // if not then the input needs to build
+}
+
+
+
+//------- Actions to run on startup
 
 // Add listeners to the inputs and use event delegation for better performance
 const divButtons = document.querySelector(".buttons");
 divButtons.addEventListener("click", delegateButtons);
+divButtons.addEventListener("update", updateDisplay);
