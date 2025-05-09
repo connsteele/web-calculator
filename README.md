8f2ce3ca5bca3d9a10bc7357bb67e2c592d8e2b4
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-09 16:37:12 -0700

    Event listener for buttons using event delegation
    All buttons are listened to with only one addEventListener call

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Calculator made with HTML, CSS , and JS
 
 ## Running Tests
-Jest is the testing framework.
+Jest is the testing framework.<br>
 `npm install`
-then
+then<br>
 `npm test` or `npm test testname`
--- js/interaction.js
+++ js/interaction.js
@@ -1,1 +1,18 @@
 import * as calc from "./calculator.js"
+
+const CHAR_OPERATOR = new Set(["+", "-", "*", "/"]);
+
+// Delegate actions depending on the calculator button pressed
+function delegateButtons(event){
+    const element = event.target;
+
+    // Reject clicks on other UI elements
+    if (!element.matches("button"))
+        return;
+
+    console.log(`${element.textContent} was clicked`);
+}
+
+// Add listeners to the inputs and use event delegation for better performance
+const divButtons = document.querySelector(".buttons");
+divButtons.addEventListener("click", delegateButtons);
