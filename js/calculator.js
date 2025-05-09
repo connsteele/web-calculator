bdcb0e1bd329f6600070319fbd655e4733ec337c
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-09 16:09:51 -0700

    Wrap calculator.js in UMD
    Resolve module issues so calculator.js can be used with both Jest and interactions.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,49 +1,55 @@
-// add function
-function add(x, y) {
-    return x + y;
-}
-
-// subtract function
-function subtract(x, y) {
-    return x - y;
-}
-
-// multiply function
-function multiply(x, y) {
-    return x * y;
-}
-
-// divide function
-function divide(x, y) {
-    return x / y;
-}
-
-// Call the appropriate operator and pass along the arguements
-function operator(op, x, y) {
-    switch (op) {
-        case "+":
-            return add(x, y);
-            break;
-        case "-":
-            return subtract(x,y);
-            break;
-        case "*":
-            return multiply(x, y);
-            break;
-        case "/":
-            return divide(x, y);
-            break;
-        default:
-            console.alert("Error, input operator does not exist");
+// UMD wrapper allows for CommonJS or Browser for registering the module correctly
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // Node / Jest
+    module.exports = factory();
+  } else {
+    // Browser global
+    root.calc = factory();
+  }
+}(
+  typeof self !== 'undefined' ? self : this,
+  function () {
+    // add function
+    function add(x, y) {
+        return x + y;
     }
-}
 
+    // subtract function
+    function subtract(x, y) {
+        return x - y;
+    }
 
-// Export functions that need to be called elsewhere
-module.exports = {
-    add,
-    subtract,
-    multiply,
-    divide,
-    operator
-};
+    // multiply function
+    function multiply(x, y) {
+        return x * y;
+    }
+
+    // divide function
+    function divide(x, y) {
+        return x / y;
+    }
+
+    // Call the appropriate operator and pass along the arguements
+    function operator(op, x, y) {
+        switch (op) {
+            case "+":
+                return add(x, y);
+                break;
+            case "-":
+                return subtract(x,y);
+                break;
+            case "*":
+                return multiply(x, y);
+                break;
+            case "/":
+                return divide(x, y);
+                break;
+            default:
+                console.alert("Error, input operator does not exist");
+        }
+    }
+
+    return { add, subtract, multiply, divide, operator };
+  }
+));
